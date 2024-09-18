5e946e6dff505118f14c003a6cbe41e82c9ec03b
Abdullah Al Alif <abdullahalif0202@gmail.com>
2024-09-18 23:51:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function MultiplicationTable() {
+    const number = document.getElementById('numberInput').value;
+    const resultDiv = document.getElementById('result');
+
+    const num = Number(number);
+
+
+    let table = `\n Multiplication table for ${num}:\n\n`;
+    for (let i = 1; i <= 10; i++) {
+        table += `${num} x ${i} = ${num * i}\n`;
+    }
+
+    
+    resultDiv.textContent = table;
+}document.getElementById('generateBtn').addEventListener('click', MultiplicationTable);
